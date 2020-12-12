250d8f459d1831df5ea2bf45cf2e627cf0cdbe07
Grigoriy Chernish <cga.jsdev@gmail.com>
2020-12-12 20:59:49 +0200

    Закончил

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import shortid from 'shortid';
+import s from './App.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Section from './UI/Section/Section';
+import Container from './UI/Container/Container';
+import ContactForm from './components/ContactForm/ContactForm';
+import Filter from './components/Filter/Filter';
+import ContactList from './components/ContactList/ContactList';
+import Modal from './UI/Modal/Modal';
+import IconButton from './UI/IconButton/IconButton';
+import { ReactComponent as AddIcon } from './icons/businessman.svg';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+    showModal: false,
+  };
+  componentDidMount() {
+    this.setState({ contacts: JSON.parse(localStorage.getItem('contacts')) });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  formSubmitHandler = ({ name, number }) => {
+    const newContact = {
+      id: shortid.generate(),
+      name,
+      number,
+    };
+    const { contacts } = this.state;
+
+    if (
+      contacts.find(
+        contact => contact.name.toLowerCase() === name.toLowerCase(),
+      )
+    ) {
+      alert(`${name} is already in contacts.`);
+    } else if (contacts.find(contact => contact.number === number)) {
+      alert(`${number} is already in contacts.`);
+    } else if (name.trim() === '' || number.trim() === '') {
+      alert("Enter the contact's name and number phone!");
+    } else if (!/\d{3}[-]\d{2}[-]\d{2}/g.test(number)) {
+      alert('Enter the correct number phone!');
+    } else {
+      this.setState(({ contacts }) => ({
+        contacts: [newContact, ...contacts],
+      }));
+    }
+    this.toggleModal();
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  changeFilter = event => {
+    this.setState({ filter: event.currentTarget.value });
+  };
+
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({
+      showModal: !showModal,
+    }));
+  };
+
+  render() {
+    const { filter, showModal } = this.state;
+    const normalazidFilter = this.state.filter.toLowerCase();
+    const filteredContacts = this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalazidFilter),
+    );
+    return (
+      <Section>
+        <Container>
+          <h1 className={s.Title}>Phonebook</h1>
+          <p className={s.Text}>
+            Add Contact:{' '}
+            <IconButton
+              onClick={this.toggleModal}
+              aria-label="Добавить контакт"
+            >
+              <AddIcon width="30" height="30" />
+            </IconButton>
+          </p>
+
+          {showModal && (
+            <Modal onClose={this.toggleModal}>
+              <ContactForm onSubmit={this.formSubmitHandler} />
+            </Modal>
+          )}
+          <h2 className={s.Title}>Contacts</h2>
+          <Filter filter={filter} onChange={this.changeFilter} />
+          <ContactList
+            contacts={filteredContacts}
+            onDeleteContact={this.deleteContact}
+          />
+        </Container>
+      </Section>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import s from './ContactForm.module.css';
+
+class ContactForm extends Component {
+  static propTypes = {
+    handleSubmit: PropTypes.func,
+  };
+
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleChange = event => {
+    const { name, value } = event.currentTarget;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state);
+    this.props.onSubmit(this.state);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({
+      name: '',
+      number: '',
+    });
+  };
+
+  render() {
+    return (
+      <form className={s.Form} onSubmit={this.handleSubmit}>
+        <label className={s.Label}>
+          Name{' '}
+          <input
+            className={s.Input}
+            type="text"
+            name="name"
+            value={this.state.name}
+            onChange={this.handleChange}
+            placeholder="Sidr Sidorov"
+          />
+        </label>
+        <label className={s.Label}>
+          Number{' '}
+          <input
+            className={s.Input}
+            type="phone"
+            name="number"
+            value={this.state.number}
+            onChange={this.handleChange}
+            placeholder="777-77-77"
+          />
+        </label>
+        <button type="submit">Add Contact</button>
+      </form>
+    );
+  }
+}
+
+export default ContactForm;
